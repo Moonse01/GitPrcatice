84c8adec9ac17989a20181c35dac1a6661f08de0
typho <typho@msy>
2025-12-14 08:54:28 +0900

    Alice branch modified class A and C

--- GitPractice/src/mygit/A.java
+++ GitPractice/src/mygit/A.java
@@ -3,5 +3,6 @@ package mygit;
 public class A {
     public void printName() {
         System.out.println("This is class A base version.");
+        System.out.println("A 클래스 - Alice가 수정함");
     }
 }
--- GitPractice/src/mygit/C.java
+++ GitPractice/src/mygit/C.java
@@ -3,5 +3,6 @@ package mygit;
 public class C {
     public void feature() {
         System.out.println("Base feature of C.");
+        System.out.println("C 클래스 - Alice가 메인 기능 수정함");
     }
 }
