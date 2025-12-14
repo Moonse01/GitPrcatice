d19bfa438cb79334d2fe7710afe435729f6f043d
typho <typho@msy>
2025-12-14 09:07:54 +0900

    Bob branch modified class A and B

--- GitPractice/src/mygit/A.java
+++ GitPractice/src/mygit/A.java
@@ -3,5 +3,6 @@ package mygit;
 public class A {
     public void printName() {
         System.out.println("This is class A base version.");
+        System.out.println("B 클래스 - Bob 브랜치에서 수정함");
     }
 }
